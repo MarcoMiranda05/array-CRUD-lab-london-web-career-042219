97401c7ca1c36cb95ba1c1ad9e3e23bed8f609e5
Marco Miranda <marmirand@hotmail.com>
2019-03-31 19:19:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   teams = ["Sao Paulo", "Corinthians", "Santos", "Palmeiras"]
   teams << "Portuguesa"
+  end
 end
 
 def add_element_to_start_of_array(array, element)
