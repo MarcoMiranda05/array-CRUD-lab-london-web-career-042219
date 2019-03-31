eba2ef852206a8717c00cf9ea62422d78e17c8f6
Marco Miranda <marmirand@hotmail.com>
2019-03-31 19:16:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Sao Paulo", "Corinthians", "Santos", "Palmeiras"]
+  teams = ["Sao Paulo", "Corinthians", "Santos", "Palmeiras"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push ("Portuguesa")
+  teams.push ("Portuguesa")
 end
 
 def add_element_to_start_of_array(array, element)
